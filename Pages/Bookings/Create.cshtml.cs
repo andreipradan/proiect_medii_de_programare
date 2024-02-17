b3ebcf829af152b4e99534acc59a45d03cd77fcc
Andrei Pradan <work@pradan.dev>
2024-02-17 20:59:23 +0200

    Book specific rooms from rooms list

--- Pages/Bookings/Create.cshtml.cs
+++ Pages/Bookings/Create.cshtml.cs
@@ -1,23 +1,32 @@
+using Hotel.Data;
+using Hotel.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Hotel.Models;
 
 namespace Hotel.Pages.Bookings
 {
     public class CreateModel : PageModel
     {
-        private readonly Hotel.Data.ApplicationDbContext _context;
-
-        public CreateModel(Hotel.Data.ApplicationDbContext context)
+        private readonly ApplicationDbContext _context;
+        public CreateModel(ApplicationDbContext context)
         {
             _context = context;
         }
-
+        
+        [BindProperty(SupportsGet = true)]
+        public int RoomIdFromQuery { get; set; }
         public IActionResult OnGet()
         {
+            if (!string.IsNullOrEmpty(Request.Query["id"]) && int.TryParse(Request.Query["id"], out int roomIdInt))
+            {
+                RoomIdFromQuery = roomIdInt;
+            }
+            else
+            {
+                ViewData["RoomId"] = new SelectList(_context.Room.ToList(), "Id", "DisplayName");
+            }
             ViewData["GuestId"] = new SelectList(_context.Users, "Id", "Email");
-            ViewData["RoomId"] = new SelectList(_context.Room.ToList(), "Id", "DisplayName");
             return Page();
         }
 
@@ -28,23 +37,30 @@ namespace Hotel.Pages.Bookings
         public async Task<IActionResult> OnPostAsync()
         {
           if (!ModelState.IsValid || _context.Booking == null || Booking == null)
-            {
-                foreach (var modelState in ModelState.Values)
-                {
-                    foreach (var error in modelState.Errors)
-                    {
-                        Console.Write(error.ErrorMessage);
-                    }
-                }
-                ViewData["GuestId"] = new SelectList(_context.Users, "Id", "Email");
-                ViewData["RoomId"] = new SelectList(_context.Room.ToList(), "Id", "DisplayName");
-                return Page();
-            }
+          {
+              foreach (var modelState in ModelState.Values)
+              {
+                  foreach (var error in modelState.Errors)
+                  {
+                      Console.Write(error.ErrorMessage);
+                  }
+              }
+              ViewData["GuestId"] = new SelectList(_context.Users, "Id", "Email");
+              if (!string.IsNullOrEmpty(Request.Query["id"]) && int.TryParse(Request.Query["id"], out int roomIdInt))
+              {
+                  RoomIdFromQuery = roomIdInt;
+              }
+              else
+              {
+                  ViewData["RoomId"] = new SelectList(_context.Room.ToList(), "Id", "DisplayName");
+              }
+              return Page();
+          }
 
-            _context.Booking.Add(Booking);
-            await _context.SaveChangesAsync();
+          _context.Booking.Add(Booking);
+          await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+          return RedirectToPage("./Index");
         }
     }
 }
