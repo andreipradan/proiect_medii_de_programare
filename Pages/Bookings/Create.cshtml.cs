344076bef7bfb97b0c1b4a45b505063fba8bda30
Andrei Pradan <work@pradan.dev>
2024-02-17 21:14:12 +0200

    validation for conflicting bookings

--- Pages/Bookings/Create.cshtml.cs
+++ Pages/Bookings/Create.cshtml.cs
@@ -3,6 +3,7 @@ using Hotel.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hotel.Pages.Bookings
 {
@@ -36,31 +37,46 @@ namespace Hotel.Pages.Bookings
 
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.Booking == null || Booking == null)
-          {
-              foreach (var modelState in ModelState.Values)
-              {
-                  foreach (var error in modelState.Errors)
-                  {
-                      Console.Write(error.ErrorMessage);
-                  }
-              }
-              ViewData["GuestId"] = new SelectList(_context.Users, "Id", "Email");
-              if (!string.IsNullOrEmpty(Request.Query["id"]) && int.TryParse(Request.Query["id"], out int roomIdInt))
-              {
+            if (Booking.CheckInDate >= Booking.CheckOutDate)
+            {
+                ModelState.AddModelError("Booking.CheckInDate", "Check-in date must be before check-out date.");
+                ModelState.AddModelError("Booking.CheckOutDate", "Check-out date must be after check-in date.");
+            }
+
+            var conflicting = _context.Booking.FirstOrDefault(b =>
+                b.RoomId == Booking.RoomId && b.CheckInDate < Booking.CheckOutDate &&
+                 b.CheckOutDate > Booking.CheckInDate);
+            if (conflicting != null)
+            {
+                ModelState.AddModelError("Booking.CheckInDate", "There is a conflicting booking: " + conflicting.CheckInDate.ToShortDateString() + "-" + conflicting.CheckOutDate.ToShortDateString());
+            }
+            if (!ModelState.IsValid || _context.Booking == null || Booking == null)
+            {
+                foreach (var modelState in ModelState.Values)
+                {
+                    foreach (var error in modelState.Errors)
+                    {
+                        Console.Write(error.ErrorMessage);
+                        
+                    }
+                    
+                }
+                ViewData["GuestId"] = new SelectList(_context.Users, "Id", "Email");
+                if (!string.IsNullOrEmpty(Request.Query["id"]) && int.TryParse(Request.Query["id"], out int roomIdInt))
+                {
                   RoomIdFromQuery = roomIdInt;
-              }
-              else
-              {
+                }
+                else
+                {
                   ViewData["RoomId"] = new SelectList(_context.Room.ToList(), "Id", "DisplayName");
-              }
-              return Page();
-          }
+                }
+                return Page();
+            }
 
-          _context.Booking.Add(Booking);
-          await _context.SaveChangesAsync();
+            _context.Booking.Add(Booking);
+            await _context.SaveChangesAsync();
 
-          return RedirectToPage("./Index");
+            return RedirectToPage("./Index");
         }
     }
 }
